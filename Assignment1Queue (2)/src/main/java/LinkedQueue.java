8ee4f5ff33497fcee98d027dd12a896dfaf461ea
Patrick O Grady <62576461+patrickog29@users.noreply.github.com>
2020-05-29 11:53:14 +0100

    Add files via upload
    A queue is a container data structure that acts like a pipeline. That is, data enters one end of the pipe and is removed from the other end (called FIFO: first in, first out). A stack is similar to a queue except the data is removed (pop) from the same end it is inserted into (push) (called LIFO: last in, first out). Data cannot be removed from an empty queue nor from an empty stack. Also links which are removed from the list should be deleted from memory.
    Build a menu-controlled application using various classes and linked lists (stacks and queues) and string data to 
    1) add data to the input end of the queue, 
    2) remove data from the output end of the queue 
       (also print each value as it is removed),
    3) clear a queue of its contents, 
    4) print the contents of the queue, 
    5) reverse the data inside the queue (create a temporary linked stack, remove the data from   the queue and place it into the stack, then remove it from the stack and place it back into the queue). (There are other ways to do this…do it this way for practice.)
    6) exit from the application
    
    The GUI that you design is totally your decision.

---
+++ Assignment1Queue (2)/src/main/java/LinkedQueue.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author PoG
+ */
+class LinkedQueue {
+
+    void append(Object inObj) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    String front() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Object serve() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
